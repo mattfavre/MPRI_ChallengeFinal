177c41b7c6da7222a679fd94fa9866fdffd3d764
mattfavre <mattfavre@hotmail.com>
2015-06-06 13:38:50 +0200

    commit de tout pour faire le rapport..commit de tout pour faire le rapport....

--- HMM_Matlab/DummyAlgo.m
+++ HMM_Matlab/DummyAlgo.m
@@ -8,8 +8,8 @@ clearvars;
 clc;
 
 % --- /!\ CONSTANT /!\ --- 
-PATH_TRAINSET_FILE      = 'C:\LocalPathToDataset\Dataset_segmented.mat'; %the path to the dataset
-PATH_TESTSET_FILE      = 'C:\LocalPathToDataset\Dataset_segmented.mat'; %for testing purposes only (Normally you should split your Trainset (cross-validation, etc))
+PATH_TRAINSET_FILE      = 'C:\Users\Matthieu\Documents\MSE\Semestre4\02_MPRI\Git\MPRI_ChallengeFinal\HMM_Matlab\DataSet\Dataset_segmented.mat'; %the path to the dataset
+PATH_TESTSET_FILE      = 'C:\Users\Matthieu\Documents\MSE\Semestre4\02_MPRI\Git\MPRI_ChallengeFinal\HMM_Matlab\DataSet\Dataset_segmented.mat'; %for testing purposes only (Normally you should split your Trainset (cross-validation, etc))
 
 % --- /!\ OPTIONS /!\ --- 
 % Choose according to what you want to do
@@ -20,7 +20,9 @@ RECOGNIZE       = 1; % 0: No, 1: Yes
 PARAMETERS = [];
 
 %Features to be extracted from the xsens sensor
-FeaturesToExtract = [1:9 18:29]; %Select which features to extract
+FeaturesToExtract =    [(1*17+7):(1*17+9) (1*17+14):(1*17+17)
+                        (2*17+7):(2*17+9) (2*17+14):(2*17+17)
+                        (3*17+7):(3*17+9) (3*17+14):(3*17+17)]; %Select which features to extract
 
 % --- LOAD TRAIN SET ORDERED BY CLASSES --------------------------%
 if LOAD_DATASET == 1
